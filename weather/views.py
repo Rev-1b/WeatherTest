6c69e5583d2b3076e3a1b56d3c59f6a4445fb259
Alex Luenko <new.luenkoaleksei@gmail.com>
2024-07-17 18:02:15 +0400

    Added forecast section

--- weather/views.py
+++ weather/views.py
@@ -17,18 +17,31 @@ class IndexView(View):
         return CityModel.objects.all()
 
     def get_context_data(self, **kwargs):
-        response = kwargs.get('response')
+        weather = kwargs.get('weather')
+        forecast = kwargs.get('forecast')
         form = kwargs.get('form')
         context = {
-            'weather_icon': response['weather'][0]['icon'],
-            'temp': round(response['main']['temp']),
-            'feels_like': round(response['main']['feels_like']),
-            'time': get_current_time_in_timezone(response['timezone']),
-            'sunrise': convert_unix_timestamp_to_local_time(response['sys']['sunrise'], response['timezone']),
-            'sunset': convert_unix_timestamp_to_local_time(response['sys']['sunset'], response['timezone']),
-            'name': response['name'], 'is_authenticated': False,
             'form': form,
+            'weather': {
+                'weather_icon': weather['weather'][0]['icon'],
+                'temp': round(weather['main']['temp']),
+                'feels_like': round(weather['main']['feels_like']),
+                'time': get_current_time(weather['timezone']),
+                'sunrise': convert_unix_to_local(weather['sys']['sunrise'], weather['timezone']),
+                'sunset': convert_unix_to_local(weather['sys']['sunset'], weather['timezone']),
+                'name': weather['name'], 'is_authenticated': False,
+            },
+            'forecast': [],
         }
+
+        for elem in forecast['list']:
+            result = {
+                'time': convert_unix_to_local(elem['dt'], weather['timezone']),
+                'temp': elem['main']['temp'],
+                'feels_like': elem['main']['feels_like'],
+                'weather_icon': elem['weather'][0]['icon'],
+            }
+            context['forecast'].append(result)
 
         if self.request.user.is_authenticated:
             context['cities'] = self.get_queryset().filter(user=self.request.user)
@@ -37,16 +50,23 @@ class IndexView(View):
         return context
 
     def get(self, request):
-        city = 'Moscow'
+        city_name = 'Moscow'
         form = CityForm()
-        response = get_city_info(city)
-        return render(request, self.template_name, self.get_context_data(response=response.json(), form=form))
+
+        weather = get_city_info(city_name).json()
+        forecast = get_city_forecast(city_name).json()
+
+        return render(request, self.template_name, self.get_context_data(
+            form=form, weather=weather, forecast=forecast
+        ))
 
     def post(self, request, *args, **kwargs):
         form = CityForm(request.POST)
         if form.is_valid():
             city_name = form.cleaned_data['city_name']
-            response = get_city_info(city_name)
+
+            weather = get_city_info(city_name).json()
+            forecast = get_city_forecast(city_name).json()
 
             user = request.user if request.user.is_authenticated else None
             city = self.get_queryset().create(name=city_name)
@@ -54,18 +74,20 @@ class IndexView(View):
             if user is not None:
                 user.cities.add(city)
 
-            return render(request, self.template_name, self.get_context_data(response=response.json(), form=form))
+            return render(request, self.template_name, self.get_context_data(
+                form=form, weather=weather, forecast=forecast
+            ))
         return HttpResponseBadRequest('Неправильный город')
 
 
-def get_current_time_in_timezone(offset_seconds):
+def get_current_time(offset_seconds):
     offset_hours = offset_seconds // 3600
     tz = pytz.FixedOffset(offset_hours * 60)
     current_time = datetime.now(tz)
     return current_time.strftime('%H:%M:%S')
 
 
-def convert_unix_timestamp_to_local_time(unix_timestamp, offset_seconds):
+def convert_unix_to_local(unix_timestamp, offset_seconds):
     offset_hours = offset_seconds // 3600
     tz = pytz.FixedOffset(offset_hours * 60)
 
@@ -85,14 +107,12 @@ def get_city_info(city):
 
 
 def get_city_forecast(city):
-    current_weather_url = 'http://api.openweathermap.org/data/2.5/weather'
+    current_weather_url = 'http://api.openweathermap.org/data/2.5/forecast'
     api_key = '1600667aec76cc2ab680ca18f43b89c0'
     params = {
         'q': city,
         'appid': api_key,
+        'cnt': 3,
         'units': 'metric',
     }
     return requests.get(current_weather_url, params=params)
-
-
-
